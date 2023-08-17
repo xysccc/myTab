f49f26625678c7e949f8bd761f9f1a7a373214c5
YuShuXiao <949516815@qq.com>
2023-08-17 11:04:41 +0800

    init

---
+++ app.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <div class="bg-sky-700 px-4 py-2">{{ title }}</div>
+    <!-- <NuxtWelcome /> -->
+  </div>
+</template>
+<script lang="ts" setup>
+const year = 2023
+const title = `${year} Nuxt3.js`
+</script>
